137918dba7cf597b1b5ede25c21d173bbe068576
tickleman <mc@thgbm.net>
2011-11-30 21:49:47 +0100

    empty RealPlugin template

---
+++ src/fr/crafter/tickleman/realviewdonttouch/RealViewDontTouchPlugin.java
@@ -0,0 +1,9 @@
+package fr.crafter.tickleman.realviewdonttouch;
+
+import fr.crafter.tickleman.realplugin.RealPlugin;
+
+//######################################################################### RealViewDontTouchPlugin
+public class RealViewDontTouchPlugin extends RealPlugin
+{
+
+}
